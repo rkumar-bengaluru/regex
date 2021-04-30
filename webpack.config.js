8db40aeac8be8df645253961e8ac6cc75c059123
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-04-30 16:12:14 +0530

    v0.1

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     entry: ['./src/app.js', './src/sass/regex.scss'],
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'js'),
         filename: 'main.bundle.js'
     },
     module: {
