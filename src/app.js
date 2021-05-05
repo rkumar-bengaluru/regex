2a37f0ebac78b18ce651affcdc781b51d87c29fb
rkumar-bengaluru <rupak.kumar.ambasta@gmail.com>
2021-05-05 19:17:44 +0530

    refactor

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@
 import Expression from "./Expression";
 import TestText from "./TestText";
 import JsonFormat from "./json/JsonFormat"
+import Icon from './img/01.jpg';
+
 let text = new TestText();
 let app = new Expression(text);
 let json = new JsonFormat();
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,53 @@
 var path = require('path');
-var webpack = require('webpack');
+
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: ['./src/app.js', './src/sass/regex.scss'],
+    mode: 'development',
+    entry: {
+        app: {
+            import: './src/app.js',
+        },
+        style: {
+            import: './src/sass/regex.scss'
+        }
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        moduleIds: 'deterministic',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/regex.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "src/img", to: "img" }
+            ],
+            options: {
+                concurrency: 100,
+            },
+        }),
+    ],
     output: {
-        path: path.resolve(__dirname, 'js'),
-        filename: 'main.bundle.js'
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [
@@ -19,16 +60,15 @@ module.exports = {
                     "sass-loader"
                 ],
             },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
     stats: {
         colors: true
     },
     devtool: 'source-map',
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        })
-    ]
+
 };
